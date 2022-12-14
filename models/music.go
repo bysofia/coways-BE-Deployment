377621f1805cdbc4d4f291a4c18607e6c62d2575
Rizky Abdulah <90577776+bysofia@users.noreply.github.com>
2022-12-14 18:53:50 +0700

    Update music.go

--- models/music.go
+++ models/music.go
@@ -4,7 +4,7 @@ type Music struct {
 	ID        int    `json:"id" gorm:"primary_key:auto_increment"`
 	Title     string `json:"title" form:"title" gorm:"type: varchar(255)"`
 	Thumbnail string `json:"thumbnail" form:"thumbnail" gorm:"type: varchar(255)"`
-	Year      int    `json:"year" form:"year" gorm:"type: varchar(255)"`
+	Year      int    `json:"year" form:"year" gorm:"type: int"`
 	SingerID  int    `json:"singer_id" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Singer    Singer `json:"singer" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	MusicFile string `json:"music_file" form:"music_file" gorm:"type: varchar(255)"`
