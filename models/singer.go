484c77d72a1b5d619a19107b531e5cb00dc43e9f
Rizky Abdulah <90577776+bysofia@users.noreply.github.com>
2022-12-14 18:51:56 +0700

    Update singer.go

--- models/singer.go
+++ models/singer.go
@@ -3,8 +3,8 @@ package models
 type Singer struct {
 	ID          int    `json:"id" gorm:"primary_key:auto_increment"`
 	Name        string `json:"name" form:"name" gorm:"type: varchar(255)"`
-	Old         int    `json:"old" form:"old" gorm:"type: varchar(255)"`
+	Old         int    `json:"old" form:"old" gorm:"type: int"`
 	Category    string `json:"category" form:"category" gorm:"type: varchar(255)"`
-	StartCareer int    `json:"start_career" form:"start_career" gorm:"type: varchar(255)"`
+	StartCareer int    `json:"start_career" form:"start_career" gorm:"type: int"`
 	Thumbnail   string `json:"thumbnail" form:"thumbnail" gorm:"type: varchar(255)"`
 }
